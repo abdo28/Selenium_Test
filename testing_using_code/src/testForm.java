e8d08227078283604926052d051dc1a624fc308e
abdo28 <abedalkhaleq28@gmail.com>
2020-09-20 07:23:46 +0300

    update 2

--- testing_using_code/src/testForm.java
+++ testing_using_code/src/testForm.java
@@ -10,12 +10,45 @@ public class testForm {
     WebDriver driver;
 
     @Test
-    public void firstScenario(){
+    public void firstScenario() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\hp\\Downloads\\chromedriver_win32\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAN__tQlYTdURExDM0ZZQVBZQzIyRjQzMjNMTFk3RTYzMy4u");
-        WebElement sendButton = driver.findElement(By.xpath("//*[@id=\"form-container\"]/div/div/div[1]/div/div[1]/div[2]/div[3]/div/div/button/div"));
-
+        WebElement NumberOneToTen = driver.findElement(By.xpath("//*[@id=\"form-container\"]/div/div/div/div/div[2]/div[2]/div[2]/div/div[2]/div/div/input"));
+        NumberOneToTen.sendKeys("11");
+        WebElement returned = driver.findElement(By.xpath("//*[@id=\"form-container\"]/div/div/div/div/div[2]/div[2]/div[2]/div/div[2]/div/div[2]/span"));
+        Assert.assertTrue(returned.isDisplayed());
         driver.close();
     }
+
+    @Test
+    public void secondScenario() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\hp\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAN__tQlYTdURExDM0ZZQVBZQzIyRjQzMjNMTFk3RTYzMy4u");
+        WebElement button = driver.findElement(By.xpath("//*[@id=\"form-container\"]/div/div/div/div/div[2]/div[3]/div[1]/button/div"));
+        button.click();
+        WebElement returned = driver.findElement(By.xpath("//*[@id=\"form-container\"]/div/div/div/div/div[2]/div[4]/div/span"));
+        Assert.assertTrue(returned.isDisplayed());
+        driver.close();
+    }
+
+    @Test
+    public void thirdScenario() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\hp\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAN__tQlYTdURExDM0ZZQVBZQzIyRjQzMjNMTFk3RTYzMy4u");
+
+        WebElement required = driver.findElement(By.xpath("//*[@id=\"form-container\"]/div/div/div/div/div[2]/div[2]/div[3]/div/div[2]/div/div/input"));
+        required.sendKeys("filled Text Field");
+
+        WebElement button = driver.findElement(By.xpath("//*[@id=\"form-container\"]/div/div/div/div/div[2]/div[3]/div[1]/button/div"));
+        button.click();
+        WebElement returned = driver.findElement(By.xpath("//*[@id=\"form-container\"]/div/div/div/div/div[1]/div[2]/div[1]/div[1]/span"));
+        Assert.assertEquals(returned.getText(), "Thanks!");
+        driver.close();
+    }
+
+
+
 }
